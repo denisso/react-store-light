d3814649061cf48e56f7971b99bac01bcb467502
Denis (mr_dramm) <like-nix@mail.ru>
2026-02-13 11:21:31 +0300

    test: useState with Context value Id param

--- __tests__/hooks/use-state.test.tsx
+++ __tests__/hooks/use-state.test.tsx
@@ -4,7 +4,7 @@ import { render, act } from '@testing-library/react';
 import Light from '../../src';
 
 describe('useState', () => {
-  it('test hook use State', () => {
+  it('useState with Store param', () => {
     type Counter = { count: number };
     const store = Light.createStore<Counter>({ count: 0 });
     let trigger!: () => void;
@@ -25,6 +25,41 @@ describe('useState', () => {
     };
 
     render(<TestComponent />);
+    expect(countTest).toBe(0);
+
+    act(() => {
+      trigger();
+    });
+
+    expect(countTest).toBe(2);
+  });
+
+  it('useState with Context value Id param', () => {
+    type Counter = { count: number };
+    const store = Light.createStore<Counter>({ count: 0 });
+    let trigger!: () => void;
+    let countTest = 0;
+    const storeId = Light.createContextValueId<Light.Store<Counter>>();
+    const TestComponent = () => {
+      const [count, setCount] = Light.useState(storeId, 'count');
+
+      React.useEffect(() => {
+        // ! test it
+        countTest = count;
+      }, [count]);
+
+      trigger = () => {
+        setCount(2);
+      };
+
+      return null;
+    };
+
+    render(
+      <Light.Provider value={{ [storeId]: store }}>
+        <TestComponent />
+      </Light.Provider>,
+    );
     expect(countTest).toBe(0);
 
     act(() => {
