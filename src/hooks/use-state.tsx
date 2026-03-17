0580c72941c600cf75dd5707380658ed5a2541c2
Denis (mr_dramm) <like-nix@mail.ru>
2026-03-17 20:06:22 +0300

    feat: additional parametr callback for transorm data

--- src/hooks/use-state.tsx
+++ src/hooks/use-state.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Store, Listener } from '../store';
 import { Aliases } from '../aliases';
-
 export function useState<T extends Record<string, any>, K extends keyof T>(
   aliases: Aliases<T>,
   key: K,
 ): T[K];
 
+export function useState<T extends Record<string, any>, K extends keyof T, R>(
+  aliases: Aliases<T>,
+  key: K,
+  cb: (value: T[K]) => R,
+): R;
+
 export function useState<T extends object, K extends keyof T>(store: Store<T>, key: K): T[K];
 
+export function useState<T extends object, K extends keyof T, R>(
+  store: Store<T>,
+  key: K,
+  cb: (value: T[K]) => R,
+): R;
 /**
  * Subscribes a component to a single store field by key.
  *
@@ -18,10 +28,11 @@ export function useState<T extends object, K extends keyof T>(store: Store<T>, k
  * @param store - Store<T> | IContextValueId<Store<T>>
  * @param key - name field in the store
  */
-export function useState<T extends Record<string, any>, K extends keyof T>(
+export function useState<T extends Record<string, any>, K extends keyof T, R = T[K]>(
   stateHolder: Store<T> | Aliases<T>,
   key: K,
-): T[K] {
+  transform?: (value: T[K]) => R,
+): R {
   const [args] = React.useState(() => {
     return {
       getSnapshot() {
@@ -35,6 +46,15 @@ export function useState<T extends Record<string, any>, K extends keyof T>(
       },
     };
   });
+
   const state = React.useSyncExternalStore(args.subscribe, args.getSnapshot) as T[K];
-  return state;
+
+  const [result] = React.useState(() => {
+    return { result: transform ? transform(state) : (state as unknown as R), state };
+  });
+  if (result.state !== state) {
+    result.result = transform ? transform(state) : (state as unknown as R);
+    result.state = state;
+  }
+  return result.result;
 }
