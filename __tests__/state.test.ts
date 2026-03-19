e9bd79ac87f2b83488dd9f8d341091deacef12bd
Denis (mr_dramm) <like-nix@mail.ru>
2026-03-19 23:34:44 +0300

    test: update test for subscribe listeners

--- __tests__/state.test.ts
+++ __tests__/state.test.ts
@@ -3,6 +3,14 @@ import { State, ListenersNode, ListenersTree } from '../src/state';
 import { posts, dict as _dict, Post } from './data/posts';
 import { getPath } from '../src/helpers/get-path';
 
+const checkNode = (node: ListenersNode, nameId: bigint, path: string[]) => {
+  const children = node.children.get(nameId);
+  expect(children?.has(path[node.depth + 1])).toBe(true);
+  const nextId = children?.get(path[node.depth + 1]) as bigint;
+  expect(typeof nextId).toBe('bigint');
+  expect(node.parents.get(nextId)).toBe(nameId);
+  return nextId;
+};
 const forState = (state: State, cb: (node: ListenersNode) => void) => {
   const tree = state.listenersTree;
 
@@ -21,26 +29,12 @@ describe('state test', () => {
     const pathMeta = getPath<typeof dict>()(posts[0].id)('meta')();
     const listener = () => {};
     const unsub = state.subsribe(pathMeta, listener);
-    let nameId: bigint;
+    let nameId = state.listenersTree.parentId;
+
     forState(state, (node) => {
-      if (node.depth == -1 && node instanceof ListenersTree) {
-        const children = node.children.get(node.parentId);
-        expect(children?.has(pathMeta[node.depth + 1])).toBe(true);
-        const nextId = children?.get(posts[0].id) as bigint;
-        expect(typeof nextId).toBe('bigint');
-        expect(node.listeners.size).toBe(0);
-        nameId = nextId;
-      }
-      if (node.depth == 0) {
-        const children = node.children.get(nameId);
-        expect(children?.has(pathMeta[node.depth + 1])).toBe(true);
-        const nextId = children?.get(pathMeta[node.depth + 1]) as bigint;
-        expect(typeof nextId).toBe('bigint');
-        expect(node.parents.get(nextId)).toBe(nameId);
-        expect(node.listeners.size).toBe(0);
-        nameId = nextId;
-      }
-      if (node.depth == 1) {
+      if (node.depth < 1) {
+        nameId = checkNode(node, nameId, pathMeta);
+      } else {
         expect(node.listeners.size).toBe(1);
         const listeners = node.listeners.get(nameId);
         expect(listeners instanceof Set).toBe(true);
